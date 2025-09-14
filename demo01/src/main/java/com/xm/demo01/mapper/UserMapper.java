7bfa6e7319f29ee26844345ce61cf3061e928183
msy20020410 <dj8817117@gmail.com>
2025-09-14 21:56:54 +0800

    User类创建与结合mbp使用

---
+++ demo01/src/main/java/com/xm/demo01/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.xm.demo01.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xm.demo01.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
